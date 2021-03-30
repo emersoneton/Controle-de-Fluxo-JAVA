8b14fb4e05f9f60dd36c5a1ec26d2ea397892b6c
Emerson Luis P <54823293+emersoneton@users.noreply.github.com>
2021-03-29 21:21:19 -0300

    Criação do projeto de Controle de Fluxo

---
+++ src/Controller/Clientes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author Emerson
+ */
+public class Clientes {
+
+    private String nome = null,
+            endereco = null,
+            telefone = null,
+            cidade = null,
+            estado = null,
+            numero = null;
+    
+    
+    
+    
+
+}
