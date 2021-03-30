baf55a08a91ca8a42e2511b0b889dbd49b74447a
Emerson Luis P <54823293+emersoneton@users.noreply.github.com>
2021-03-29 21:50:37 -0300

    Primeira tela Cadastro de Cliente e classe orientada

--- src/Controller/Clientes.java
+++ src/Controller/Clientes.java
@@ -16,7 +16,66 @@ public class Clientes {
             telefone = null,
             cidade = null,
             estado = null,
-            numero = null;
+            numero = null,
+            cep = null;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    
     
     
     
